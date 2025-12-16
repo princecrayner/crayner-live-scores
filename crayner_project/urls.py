6cdfc7bd1dea8dbd5cf4af64a9c37ca86daaae3d
prince <prince@prince.prince>
2025-12-16 11:19:36 -0500

    Add homepage route

--- crayner_project/urls.py
+++ crayner_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from api.views import live_page
 
 urlpatterns = [
+    path('', live_page),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
